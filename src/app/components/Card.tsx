633a34647919dad0ae5fc8aedb08e5ba8cb2d964
Menawer <m--0@hotmail.com>
2023-12-31 07:10:02 +0300

    05-tsx

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,12 @@
+export default function Card() {
+    return (
+        <div>
+            <h1>Salam</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur perferendis nam nesciunt in eaque magni facere nulla nemo id quo veritatis vero, unde eos at sed sapiente ex quibusdam iusto?</p>
+            <ul>
+                <li>item 1</li>
+                <li>item 2</li>
+            </ul>
+        </div>
+    )
+}
