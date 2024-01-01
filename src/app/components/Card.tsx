8f0e98ee9e94b8eaf78624a20faf26955005633f
Menawer <m--0@hotmail.com>
2024-01-01 04:22:54 +0300

    07-props

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,7 +1,14 @@
-export default function Card() {
+'use client'
+
+type Props = {
+    title:string;
+    description:string
+}
+
+export default function Card(props:Props) {
     return (
         <div>
-            <h1>Salam</h1>
+            <h1>{props.title}</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur perferendis nam nesciunt in eaque magni facere nulla nemo id quo veritatis vero, unde eos at sed sapiente ex quibusdam iusto?</p>
             <ul>
                 <li>item 1</li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import Card, { MicroCard, MiniCard } from "./components/Card";
 export default function Home() {
   return (
     <div>
-      <Card />
-      <MiniCard />
-      <MicroCard />
+      <Card title="Card 1 Title" description="Card 1 Description" />
+      <Card title="Card 2 Title" description="Card 2 Description" />
+      <Card title="Card 3 Title" description="Card 3 Description" />
     </div>
   )
 }
